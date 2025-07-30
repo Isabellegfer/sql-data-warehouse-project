63a1127667d9955e5387df47cd041bc7937280a9
Isabellegfer <88055107+Isabellegfer@users.noreply.github.com>
2025-07-30 21:18:17 +0100

    Update init_database.sql
    Option of creating and restoring a backup

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -13,20 +13,47 @@ WARNING:
     and ensure you have proper backups before running this script.
 */
 
+
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+/* In case of creating a backup:
+DECLARE @BackupPath NVARCHAR(500) = 'C:\Users\IsabelleFerreira\Documents\1. Isabelle Ferreira\Formações\Data Warehouse\backups\'; -- Altere conforme o local desejado
+DECLARE @Timestamp NVARCHAR(20) = FORMAT(GETDATE(), 'yyyyMMdd_HHmmss');
+DECLARE @BackupFile NVARCHAR(600) = @BackupPath + 'DataWarehouse_Backup_' + @Timestamp + '.bak';
+*/
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
 BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
+
+/* In case of creating a backup:
+	BACKUP database DataWarehouse
+	TO DISK = @BackupPath
+	WITH FORMAT, INIT, NAME = 'Backup do DataWarehouse antes da exclusão';
+*/
+
+	ALTER database DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+	DROP database DataWarehouse;
 END;
 GO
 
--- Create the 'DataWarehouse' database
-CREATE DATABASE DataWarehouse;
+CREATE database DataWarehouse;
 GO
+
+/*
+Second option:
+
+-- Restaurar o backup no mesmo nome de banco
+RESTORE DATABASE DataWarehouse
+FROM DISK = 'C:\Backup\DataWarehouse_Backup_20250730_101500.bak'
+WITH 
+    REPLACE, -- Substitui o banco existente (se houver)
+    RECOVERY; -- Deixa o banco pronto para uso
+GO
+
+*/
+
 
 USE DataWarehouse;
 GO
